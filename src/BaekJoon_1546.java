772b1185915a385ec842509e1f480bab514cf772
Seo Eun <seo606101@naver.com>
2023-07-17 01:29:06 +0900

    commit

---
+++ src/BaekJoon_1546.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class BaekJoon_1546 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        // 과목의 개수 n
+        if(n>1000 || n<0)
+            return;
+        //0<n<=1000
+
+        int[] scores = new int[n];
+        // 점수 n개
+
+        for(int i=0; i<scores.length; i++){
+            int score = sc.nextInt();
+            scores[i] = score;
+        }
+
+        int max = -1;
+        int sum = 0;
+
+        for(int i=0; i<n; i++) {
+            if (scores[i] > max) {
+                max = scores[i];
+            }
+            //최대값 구하기
+            sum += scores[i];
+            //총합 구하기
+
+        }
+        System.out.println(sum*100.0/n/max);
+        //System.out.println(sum/max*100.0/n); 잘못된 수식
+
+    }
+}
---
+++ src/BaekJoon_2750.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BaekJoon_2750 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String chr = sc.next();
+
+        char[] StringNumber = chr.toCharArray();
+
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            sum+=Character.getNumericValue(StringNumber[i]);
+        }
+        System.out.println(sum);
+    }
+
+}
