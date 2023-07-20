15baf889be42f01b292497d78f9143f3bccea62a
Seo Eun <seo606101@naver.com>
2023-07-20 19:08:58 +0900

    백준 2018번 다른 풀이

--- src/BaekJoon_10986.java
+++ src/BaekJoon_10986.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class BaekJoon_10986 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        long [] s = new long[n];
+        long [] c = new long[n];
+
+        int cnt =0;
+
+        s[0] = sc.nextInt();
+
+        for(int i=1; i<n; i++){
+            s[i] = s[i-1] + sc.nextInt();
+        }
+
+        for(int i = 0; i<n; i++){
+            int remainder = (int) (s[i]%m);
+            c[i] = remainder;
+            System.out.println(c[i]);
+        }
+        //System.out.println(cnt);
     }
 }
